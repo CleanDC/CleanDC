41b5c7265162348c8df1f690ed091c26f5cd0e0a
Sunny Holic <cleandc@sunnyholic.com>
2019-02-19 23:52:15 +0900

    리스트에서 게시글 링크가 아닌 줄  블락

--- src/functions/elementBlock.js
+++ src/functions/elementBlock.js
@@ -2,6 +2,14 @@ import { Element } from '../../utils'
 
 export default {
   list (list) {
-    list.find(`.gall_num:contains('AD')`).parent(Element.selectors.row).hide() // 갤리스트 광고글 숨김
+    const tags = list.find(Element.selectors.row + ' .gall_tit a:not(.reply_numbox)')
+    let i = 0
+    for (; i < tags.length; i++) {
+      const tag = tags[i]
+      const { attributes: { href } } = tag
+      if (href && href.value.includes('/board/view')) break
+    }
+    tags.closest(Element.selectors.row).slice(0, i).hide()
+    // 게시글 링크가 아닌 링크가 달린 경우 광고로 판단
   }
 }
